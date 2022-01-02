ed3b6dcc4ad31b0d3673e5d50bfef07e103c9b08
Gabriel Gomides <68978413+gomideus@users.noreply.github.com>
2022-01-01 22:59:51 -0300

    Add files via upload

---
+++ C++/1-Beginner/BEE-1000.cpp
@@ -0,0 +1,14 @@
+
+/*
+NOME: Gabriel Gomides Dutra Paiva
+UNIVERSIDADE: Puc Minas
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
