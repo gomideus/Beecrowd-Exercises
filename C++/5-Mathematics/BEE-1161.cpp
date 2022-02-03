18adef79a25f047058928cf160735cc0c428804b
Gabriel Gomides <68978413+gomideus@users.noreply.github.com>
2022-02-02 21:01:19 -0300

    Update BEE-1161.cpp

--- C++/5-Mathematics/BEE-1161.cpp
+++ C++/5-Mathematics/BEE-1161.cpp
@@ -1,10 +1,8 @@
 
 /*
-NOME: Gabriel Gomides Dutra Paiva
-MATRÍCULA: 652593
-TURNO: Manhã
+Solução para calculo e soma de fatoriais a partir de n entradas.
+Complexidade: O(n2)
 */
-
 
 #include <iostream>
 #include <fstream>
@@ -14,53 +12,44 @@ using namespace std;
 
 int main(){
 
-    short int ammount;
-    short int test_ammount; 
+    long long i = 0.0; 
 
-    int length;
-    int test_length;
-    int x;
+    // Tipo de variável long long utilizado para poder representar um valor com 15 dígitos.
+    long long first_value = 0.0;
+    long long second_value = 0.0;
+    long long sum = 0.0;
 
-    string str;
-    string test_str;
+    // Enquanto houverem entradas
+    while( cin >> first_value ){
 
-    bool found;
+        cin >> second_value;
 
-    cin >> ammount;
-    for( int i = 0; i < ammount; i++ ){
+        // Se o valor for diferente de 0 ( porque fatorial de 0 é 1 )
+        if ( first_value != 0 ){
 
-        cin >> str;
-        cin >> test_ammount;
-
-        length = str.length();
-
-        for( int j = 0; j < test_ammount; j++ ){
-
-            cin >> test_str;
-            test_length = test_str.length();
-
-            found = false;
-            x = 0;
-            for( int k = 0; k < length; k++ ){ // Neste loop buscamos pelos 'matchs' dos caracteres.
-
-                if( test_str[x] == str[k] ){
-                    x++;
-                }
-
-                if( x == test_length ){
-                    found = true;
-                    break;
-                }
-
+            // Calculando o fatorial do primeiro número
+            for ( i = first_value-1; i >= 1 ; i-- ){
+                first_value = first_value * i; // Multiplica o valor sequenciamente por i
             }
 
-            if( found ){
-                cout << "Yes\n";
-            }else{
-                cout << "No\n";
-            }
-
+        }else{
+            first_value = 1;
         }
+
+        // Repetir procedimento anterior para o segundo valor de entrada
+        if ( second_value != 0 ){
+
+            for ( i = second_value-1; i >= 1 ; i-- ){
+                second_value = second_value * i;
+            }
+
+        }else{
+            second_value = 1;
+        }
+
+        sum = first_value + second_value; // Aqui somamos os dois valores fatoriais obtidos.
+
+        cout << sum << '\n'; // Mostrar resposta e pular linha.
 
     }
 
