bece4d102ab8839919f03dadc3d39d9c343b9bcf
Gabriel Gomides <68978413+gomideus@users.noreply.github.com>
2022-02-02 21:03:00 -0300

    Add files via upload

---
+++ C++/5-Mathematics/BEE-1168.cpp
@@ -0,0 +1,49 @@
+
+/*
+NOME: GABRIEL GOMIDES DUTRA PAIVA
+MATRICULA: 652593
+TURNO: MANHÃ
+
+ATIVIDADE: Led´s - C++
+Complexidade da Solução apresentada: O(n) -> Em que 'n' representa os números de entrada
+
+*/
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+// Guardar qauntidade de LED´s para cada número
+int led_vec[10] = {6, 2, 5, 5, 4, 5, 6, 3, 7, 6};
+
+int main(){
+
+    int ammount, led_counter, i = 0;
+    cin >> ammount;
+    string numero = "";
+
+    while( i < ammount ){
+
+        cin >> numero;
+
+        int length = numero.length();
+        led_counter = 0; // Reseta contador para cada string
+
+        for( int j = 0; j < length; j++ ){
+
+            char sChar = numero[j]; // Pegar char da posição atual
+            int charToInt = sChar - '0'; // Converte o char para inteiro
+            led_counter += led_vec[charToInt]; // Pular para o vetor na posição do caractere atual e somar sua respectiva quantidade de LED´s
+
+        }
+
+        cout << led_counter << " leds" << "\n";
+
+        i++;
+
+    }
+
+    return 0;
+
+}
